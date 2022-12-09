ab21dca316c1d3aa871d41a0eeaa5878dc8f3e87
huskyeart <109225421+huskyeart@users.noreply.github.com>
2022-12-10 00:54:18 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+//document.write("<H1>Iproject</H1>");  //คอมเม้นบรรทัดเดียว
+//document.write("<H1>Iproject</H1>");
+//alert("รักปู");
+
+//console.log("neme");
+
+/*.คอมเม้นหลายบรรทัด..*/ 
+
+/*console.error("ข้อผิดผลาด");
+console.warn("เตือน");*/
+
+// กำหนดตัวแปร
+
+let age;     //สร้างตัวแปรแต่ยังไม่กำหนดค่า
+
+let firstname = "heartrak", tall = 180;
+age = '29';
+console.log("Before = ",firstname);
+firstname = "Worapon";  // let สามารถหฃเปลี่ยนค่าตัวแปรได้
+console.log("After = " ,firstname) 
+document.write(firstname);
+console.log("อายุ = ",age);
+
+
+const lastname = "Pongprisansri"
+console.log("before = ",lastname)
+lastname = "Kongraksaim"  //ควร error เพราะ ตังแปร const ไม่สามารถเปลี่ยนค่าได้
+console.log("after = ", lastname)
+
+
+
